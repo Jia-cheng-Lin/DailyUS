fdf1039523d865be2e22537b81db10c8da78b262
Bill <bill092804@gmail.com>
2025-12-02 00:35:38 +0800

    Initial Commit

---
+++ DailyUS/DailyUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyUSApp.swift
+//  DailyUS
+//
+//  Created by 陳芸萱 on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct DailyUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
